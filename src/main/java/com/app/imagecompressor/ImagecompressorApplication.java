920d275cf5d88c2d860a2d27dec7a43092cf0c79
huytv <huy8895@gmail.com>
2022-09-17 10:45:30 +0700

    init

---
+++ src/main/java/com/app/imagecompressor/ImagecompressorApplication.java
@@ -0,0 +1,13 @@
+package com.app.imagecompressor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImagecompressorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImagecompressorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/app/imagecompressor/ImagecompressorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.imagecompressor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImagecompressorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

c8a859e8e908aece4e0da043271459b5e4cf7476
huytv <huy8895@gmail.com>
2022-09-18 22:17:34 +0700

    done api compress image v2 from url

--- src/main/java/com/app/imagecompressor/controller/CompressorController.java
+++ src/main/java/com/app/imagecompressor/controller/CompressorController.java
@@ -5,10 +5,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 @Slf4j
@@ -32,7 +29,7 @@ public class CompressorController {
                                @RequestParam("quality") int quality) {
         byte[] compressData = imageCompressor.compress(url, quality);
         return ResponseEntity.ok()
-//                             .contentType(MediaType.valueOf(file.getContentType()))
+                             .contentType(MediaType.IMAGE_JPEG)
                              .body(compressData);
     }
 }
